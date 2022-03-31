f3f9be98040f0d019e0deda9b9a0ea78ba441ca0
valere2709 <102733265+valere2709@users.noreply.github.com>
2022-03-31 20:28:45 +0900

    Add files via upload

---
+++ BackTestETH.py
@@ -0,0 +1,17 @@
+
+import pyupbit
+import numpy as np
+
+df = pyupbit.get_ohlcv("KRW-ETH", count=30)
+df['range'] = (df['high'] - df['low']) * 0.5
+df['target'] = df['open'] + df['range'].shift(1)
+
+fee = 0.0025
+df['ror'] = np.where(df['high'] > df['target'],
+                     df['close'] / df['target'] - fee,
+                     1)
+
+df['hpr'] = df['ror'].cumprod()
+df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+print("MDD(%): ", df['dd'].max())
+df.to_excel("dd.xlsx")
